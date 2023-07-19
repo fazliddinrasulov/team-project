3cbc77479b2fb28286a96f62df95737ed3cb85c9
userFrontend <aba06096@gmail.com>
2023-07-19 16:43:33 +0500

    set

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn Margin Name
+          😂
         </a>
       </header>
     </div>
